0394aec9984e091fa255488bb455eb1dba02c744
mmtimes-insite <mmtimes2023@gmail.com>
2025-09-16 02:03:51 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-window.addEventListener("load", function() {
-  let pre = document.getElementById('preloader');
-  if(pre){
-    pre.style.opacity = "1";
-    let fade = setInterval(()=>{
-      if(pre.style.opacity > 0){
-        pre.style.opacity -= 0.05;
+window.addEventListener("load", function () {
+  let pre = document.getElementById("preloader");
+  if (pre) {
+    pre.style.opacity = 1;
+    let fade = setInterval(() => {
+      let current = parseFloat(pre.style.opacity);
+      if (current > 0) {
+        pre.style.opacity = (current - 0.05).toString();
       } else {
         clearInterval(fade);
         pre.style.display = "none";
@@ -12,4 +13,3 @@ window.addEventListener("load", function() {
     }, 50);
   }
 });
-
