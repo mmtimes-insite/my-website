3660ce269dd64da0fff590255133443af1561f58
mmtimes-insite <mmtimes2023@gmail.com>
2025-09-16 01:41:38 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+window.addEventListener("load", function() {
+  let pre = document.getElementById('preloader');
+  if(pre){
+    pre.style.opacity = "1";
+    let fade = setInterval(()=>{
+      if(pre.style.opacity > 0){
+        pre.style.opacity -= 0.05;
+      } else {
+        clearInterval(fade);
+        pre.style.display = "none";
+      }
+    }, 50);
+  }
+});
 
